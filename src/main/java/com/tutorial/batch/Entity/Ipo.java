82337e6660da7bb5d40e946990299594a0e197f8
Basak <chgy2131@naver.com>
2022-05-25 22:54:16 +0900

    init2

--- src/main/java/com/tutorial/batch/Entity/Ipo.java
+++ src/main/java/com/tutorial/batch/Entity/Ipo.java
@@ -1,7 +1,12 @@
 package com.tutorial.batch.Entity;
 
-import lombok.*;
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,7 +15,7 @@ import java.time.format.DateTimeFormatter;
 @Setter
 @NoArgsConstructor
 @Entity
-public class Pay {
+public class Ipo {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
     @Id
@@ -18,21 +23,22 @@ public class Pay {
 
     private Long amount;
     private String txName;
+    private String name;
     private LocalDateTime txDateTime;
 
-    public Pay(Long amount, String txName, String txDateTime) {
+    public Ipo(Long amount, String txName, String txDateTime) {
         this.amount = amount;
         this.txName = txName;
         this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
     }
 
-    public Pay(Long id, Long amount, String txName, String txDateTime) {
+    public Ipo(Long id, Long amount, String txName, String txDateTime) {
         this.id = id;
         this.amount = amount;
         this.txName = txName;
         this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
     }
 
-    public Pay(Long amount, String txName, LocalDateTime txDateTime) {
+    public Ipo(Long amount, String txName, LocalDateTime txDateTime) {
     }
 }
